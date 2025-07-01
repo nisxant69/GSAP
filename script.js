f7e91231071a33019028e6ea88bd5589e599d5e5
Nishanta Khadka <nisxant69@gmail.com>
2025-07-01 23:48:37 +0545

    Initial Commit || StartedLearning GSAP

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-// Two methods "to" and "from"
-gsap.to("#box",{  //Write ID of element
-    x:1000,
-    duration:4,
+// Duita method "to" ra "from" hunxa
+
+// Initial position bata Final position laijana .to use hunxa 
+// Final position bata Initial position laijana .from use hunxa 
+
+// gsap.to("#box",{  //Write ID of element
+//     x:1200,
+//     y:600,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"50%",
+//     scale:2,
+// }) 
+// gsap.from("#box1",{  //Write ID of element
+//     x:1200,
+//     duration:5,
+//     delay:1,
+// }) 
+
+// gsap.to("#box1",{  //Write ID of element
+//     y:300,
+//     x:500,
+//     duration:4,
+//     delay:1,
+// }) 
+// gsap.from(,{})
+
+gsap.from("h1",{
+    opacity:0,
+    duration:1,
+    y:20,
     delay:2,
-}) 
-// gsap.
+    stagger:1
+})
